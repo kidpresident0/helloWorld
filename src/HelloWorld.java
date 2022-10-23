90dd8fd3458426aca3ec99f1fafd2923f44fd217
John Christian <john.christian1@pcc.edu>
2022-10-23 13:32:15 -0700

    Added date feature

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 /**
  * Author John Christian
  */
@@ -5,5 +7,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
